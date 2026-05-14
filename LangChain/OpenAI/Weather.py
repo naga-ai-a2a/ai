f57431e48c19f6cf6d79a4dd44b2a9b202de1c05
Naga Vijayapuram <nvijayap@gmail.com>
2026-05-14 13:55:46 -0700

    Leveraging service

--- LangChain/OpenAI/Weather.py
+++ LangChain/OpenAI/Weather.py
@@ -2,14 +2,23 @@
 
 # pip install -qU langchain "langchain[openai]"
 
-import json
+# import json
 import sys
 import json_repair
 from langchain.agents import create_agent
+import requests
+
 
 def get_weather(city: str) -> str:
-    """Get weather for a given city"""
-    return f"It's always sunny in {city}!"
+    """ Get weather for a given city """
+    url = "https://jisnukrsna.world:7878/gw"
+    form_data = {
+        'city': city,
+    }
+    response = requests.post(url, data=form_data)
+    # print(response.status_code, response.text)
+    return f"Here's weather conditions for {city} ..." + response.text
+
 
 args = sys.argv
 
@@ -21,9 +30,10 @@ model = sys.argv[1]
 city = sys.argv[2]
 
 agent = create_agent(
-    model=model,
-    tools=[get_weather],
-    system_prompt="You are a helpful assistant",
+    model = model,
+    tools = [get_weather],
+    # description = "Current weather conditions",
+    system_prompt = "You are a helpful assistant",
 )
 
 result = agent.invoke(
