2daaaedad60228da8ef500911911c81e2997f615
Naga Vijayapuram <nvijayap@gmail.com>
2026-05-15 06:13:47 -0700

    Added Anthropic Claude

--- LangChain/Weather.sh
+++ LangChain/Weather.sh
@@ -2,6 +2,11 @@
 
 # Weather.sh
 
+cd `dirname $0`
+
+printf "\nUsing Anthropic Claude ...\n\n"
+AnthropicClaude/Weather.sh claude-sonnet-4-6 pune
+
 printf "\nUsing Google Gemini ...\n\n"
 GoogleGemini/Weather.sh google_genai:gemini-2.5-flash-lite pune
 
