17c05bd4090f0f83241d4c3962197261f913f18d
Naga Vijayapuram <nvijayap@gmail.com>
2026-05-15 05:03:16 -0700

    Weather.sh

---
+++ LangChain/GoogleGemini/Weather.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`
+
+if [ $# -ne 2 ]; then
+    printf "\n\tNeed <model like openai:gpt-5.4> <city like pune> as args\n\n"
+    exit 9
+fi
+
+trap "rm -fr .venv" EXIT SIGINT SIGTERM
+
+uv venv --clear >/dev/null 2>&1
+
+source .venv/bin/activate
+
+uv pip install -q -r Requirements.txt
+
+python Weather.py $1 $2 | jq
